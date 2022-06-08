5a9738786e9d17ae800da2a3da97c4b03346af25
Natali Knyazeva <knyazeva14@mail.ru>
2022-06-08 19:01:47 +0700

    Create UserDaoImp.java

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,50 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        entityManager.remove(entityManager.find(User.class, user.getId()));
+    }
+
+    @Override
+    public void update(long id, User user) {
+        Query query = entityManager.
+                createNativeQuery("UPDATE users SET firstName = :firstName, lastName = :lastName, age = :age WHERE id = :id");
+        query.setParameter("firstName", user.getFirstName());
+        query.setParameter("lastName", user.getLastName());
+        query.setParameter("age", user.getAge());
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+
+    @Override
+    public List<User> listUsers() {
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u", User.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserById(long id) {
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u WHERE u.id = :id", User.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+}
